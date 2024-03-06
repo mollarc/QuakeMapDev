d4e5610162591720af52cd8ac7240c061ac291e4
mollarc <mollark@live.com>
2024-03-06 16:27:23 -0600

    1st iteration changes
    cover added. easier to get to 2nd floor. steps added. walls removed from north and south stairwells.

--- working/compile.bat
+++ working/compile.bat
@@ -1,26 +1,26 @@
 @echo off
 
 cd\
-cd C:\QuakeMapDev\working
+cd C:\Users\molla\Documents\GitHub\QuakeMapDev\working
 
 
 echo Copying Files...
-copy C:\QuakeMapDev\working\SpireOliverChen.map C:\QuakeMapDev\working
+copy C:\Users\molla\Documents\GitHub\QuakeMapDev\working\SpireOliverChen.map C:\Users\molla\Documents\GitHub\QuakeMapDev\working
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-C:\QuakeMapDev\tools\ericw-tools\bin\qbsp.exe SpireOliverChen
+C:\Users\molla\Documents\GitHub\QuakeMapDev\tools\ericw-tools\bin\qbsp.exe SpireOliverChen
 
 echo --------------VIS---------------
-C:\QuakeMapDev\tools\ericw-tools\bin\vis.exe SpireOliverChen
+C:\Users\molla\Documents\GitHub\QuakeMapDev\tools\ericw-tools\bin\vis.exe SpireOliverChen
 
-copy SpireOliverChen.bsp C:\QuakeMapDev\id1\maps
-copy SpireOliverChen.pts C:\QuakeMapDev\id1\maps
-copy SpireOliverChen.lit C:\QuakeMapDev\id1\maps
+copy SpireOliverChen.bsp C:\Users\molla\Documents\GitHub\QuakeMapDev\id1\maps
+copy SpireOliverChen.pts C:\Users\molla\Documents\GitHub\QuakeMapDev\id1\maps
+copy SpireOliverChen.lit C:\Users\molla\Documents\GitHub\QuakeMapDev\id1\maps
 pause
 cd\
-cd C:\QuakeMapDev
+cd C:\Users\molla\Documents\GitHub\QuakeMapDev
 quakespasm-spiked-win64  +map SpireOliverChen
