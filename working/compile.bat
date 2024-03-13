5ac968fe88da78de078bbb3675039ad5ef58b75b
mollarc <mollark@live.com>
2024-03-13 16:32:57 -0500

    Textures added

--- working/compile.bat
+++ working/compile.bat
@@ -17,9 +17,18 @@ C:\QuakeMapDev\tools\ericw-tools\bin\qbsp.exe SpireOliverChen
 echo --------------VIS---------------
 C:\QuakeMapDev\tools\ericw-tools\bin\vis.exe SpireOliverChen
 
+<<<<<<< Updated upstream
 copy SpireOliverChen.bsp C:\QuakeMapDev\id1\maps
 copy SpireOliverChen.pts C:\QuakeMapDev\id1\maps
 copy SpireOliverChen.lit C:\QuakeMapDev\id1\maps
+=======
+echo -------------LIGHT--------------
+C:\Users\molla\Documents\GitHub\QuakeMapDev\tools\ericw-tools\bin\light.exe SpireOliverChen
+
+copy SpireOliverChen.bsp C:\Users\molla\Documents\GitHub\QuakeMapDev\id1\maps
+copy SpireOliverChen.pts C:\Users\molla\Documents\GitHub\QuakeMapDev\id1\maps
+copy SpireOliverChen.lit C:\Users\molla\Documents\GitHub\QuakeMapDev\id1\maps
+>>>>>>> Stashed changes
 pause
 cd\
 cd C:\QuakeMapDev
